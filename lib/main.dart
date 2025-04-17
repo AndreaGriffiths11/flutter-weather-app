16624d7fa3d80e4248265d5a484959a13e3a7c0c
Andrea Liliana Griffiths <andreagriffiths11@github.com>
2025-04-16 20:52:09 -0400

    Add main.dart with basic app structure

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'services/weather_service.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => WeatherService(),
+      child: MaterialApp(
+        title: 'Flutter Weather App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          useMaterial3: true,
+        ),
+        home: const HomeScreen(),
+      ),
+    );
+  }
+}
